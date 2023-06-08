c8957b7965118d87e50f852782269f20efacd0a8
DuanNH214951 <Duan.NH214951@sis.hust.edu.vn>
2023-06-08 11:09:55 +0700

    first commit, not complete

---
+++ Code/apache-nginx/src/errorlog/LogParser.java
@@ -0,0 +1,5 @@
+package errorlog;
+
+public class LogParser {
+
+}
---
+++ Code/apache-nginx/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Nguyễn Hữu Duẩn
+ *
+ */
+module project1 {
+	requires org.json;
+	requires com.google.gson;
+}
