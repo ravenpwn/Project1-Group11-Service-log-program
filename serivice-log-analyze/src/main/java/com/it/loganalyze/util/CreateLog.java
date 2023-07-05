6fc4e92a48bd6f1527dbeac01f3b0320ba0f0bcc
Hardcorelevelingwarrior <nhuducminh123@gmail.com>
2023-07-05 18:50:01 +0700

    Add Audit.java in com.it.loganalyze.util, add createAudit and createDebug for modsecurity in CreateLog.java. Add JsonConverter in util for analyze raw audit file

--- serivice-log-analyze/src/main/java/com/it/loganalyze/util/CreateLog.java
+++ serivice-log-analyze/src/main/java/com/it/loganalyze/util/CreateLog.java
@@ -1,11 +1,14 @@
 package com.it.loganalyze.util;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 import com.google.gson.JsonObject;
 import com.it.loganalyze.log.ApacheAccessLog;
 import com.it.loganalyze.log.ApacheErrorLog;
+import com.it.loganalyze.log.Audit;
+import com.it.loganalyze.log.Debug;
 import com.it.loganalyze.log.IptablesLog;
 import com.it.loganalyze.log.Log;
 import com.it.loganalyze.log.LogData;
@@ -85,6 +88,60 @@ public class CreateLog {
 		}
 		logData = new LogData(logList);
 		return logData;
+	}
+	public static LogData createAudit(String inputFileName) {
+	    // Set a fixed output file name
+	    String outputFileName = "./log/audit.json";
+
+	    // Create a new instance of JsonConverter and convert the input file
+	    JsonConverter converter = new JsonConverter(inputFileName, outputFileName);
+	    try {
+	        converter.convert();
+	    } catch (IOException e) {
+	        System.err.println("Error converting file: " + e.getMessage());
+	    }
+
+	    // Read the converted data from the output file
+	    LogData logData;
+	    JsonObject data = Util.readJsonFile(outputFileName).getAsJsonObject();
+	    ArrayList<Log> logList = new ArrayList<>();
+
+	    for (String line : data.keySet()) {
+	        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+	        JsonObject logLine = data.get(line).getAsJsonObject();
+	        for (String key : logLine.keySet()) {
+	            String value = logLine.get(key).toString();
+	            map.put(key, value);
+	        }
+	        Log auditlog = new Audit(map);
+	        logList.add(auditlog);
+	    }
+	    logData = new LogData(logList);
+	    return logData;
+	}
+	public static LogData createDebug(String fileName) {
+		LogData logData;
+		JsonObject data = Util.readJsonFile(fileName).getAsJsonObject();
+		ArrayList<Log> logList = new ArrayList<>();
+		// logList contains logLine in the form <key, value>
+
+		for (String line : data.keySet()) {
+			// line is the index of each logLine
+			LinkedHashMap<String, String> map = new LinkedHashMap<>();
+			JsonObject logLine = data.get(line).getAsJsonObject();
+			for (String key : logLine.keySet()) {
+				// key is field in a log
+				// value is the value of that key field
+				String value = logLine.get(key).getAsString();
+				map.put(key, value);
+				// so map will be a LinkedHashMap that contains all pair <key, value> in a log
+			}
+			Log debug = new Debug(map);
+			logList.add(debug);
+		}
+		logData = new LogData(logList);
+		return logData;
+
 	}
 
 
